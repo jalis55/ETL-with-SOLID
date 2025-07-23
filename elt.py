c79c20d48f8fdee42d2819aba56d5caf84e66c81
jalis55 <jalismahamud2055@gmail.com>
2025-07-23 13:03:19 +0600

    violation of SRP

--- elt.py
+++ elt.py
@@ -1,1 +1,12 @@
-print("initail setup")
+# 🧩 S — Single Responsibility Principle (SRP)
+
+class ETLProcessor:
+    def extract(self):
+        # Connect to S3, read CSV
+        pass
+    def transform(self, data):
+        # Clean, filter, enrich
+        pass
+    def load(self, data):
+        # Insert into database
+        pass
